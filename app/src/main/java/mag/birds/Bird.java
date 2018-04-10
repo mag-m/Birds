659aeeaa23c89c20bbc76fe1db2132fad96e1b70
Mag <magnusemortensen95@gmail.com>
2018-04-10 13:54:35 +0200

    Added model classes: Observation and Bird.
    Updated GUI a bit

--- app/src/main/java/mag/birds/Bird.java
+++ app/src/main/java/mag/birds/Bird.java
@@ -1,4 +1,79 @@
 package mag.birds;
 
+import java.util.Calendar;
+
 public class Bird {
+    private Calendar Created;
+    private int Id;
+    private String NameDanish;
+    private String NameEnglish;
+    private String PhotoUrl;
+    private String UserId;
+
+
+    public Bird(){
+
+    }
+
+    public Bird(Calendar created, int id, String nameDanish, String nameEnglish, String photoUrl, String userId){
+        Created = created;
+        Id = id;
+        NameDanish = nameDanish;
+        NameEnglish = nameEnglish;
+        PhotoUrl = photoUrl;
+        UserId = userId;
+    }
+
+    public Calendar getCreated() {
+        return Created;
+    }
+
+    public void setCreated(Calendar created) {
+        Created = created;
+    }
+
+    public int getId() {
+        return Id;
+    }
+
+    public void setId(int id) {
+        Id = id;
+    }
+
+    public String getNameDanish() {
+        return NameDanish;
+    }
+
+    public void setNameDanish(String nameDanish) {
+        NameDanish = nameDanish;
+    }
+
+    public String getNameEnglish() {
+        return NameEnglish;
+    }
+
+    public void setNameEnglish(String nameEnglish) {
+        NameEnglish = nameEnglish;
+    }
+
+    public String getPhotoUrl() {
+        return PhotoUrl;
+    }
+
+    public void setPhotoUrl(String photoUrl) {
+        PhotoUrl = photoUrl;
+    }
+
+    public String getUserId() {
+        return UserId;
+    }
+
+    public void setUserId(String userId) {
+        UserId = userId;
+    }
+
+    @Override
+    public String toString() {
+        return NameDanish;
+    }
 }
--- app/src/main/java/mag/birds/Observation.java
+++ app/src/main/java/mag/birds/Observation.java
@@ -1,4 +1,131 @@
 package mag.birds;
 
+import java.util.Calendar;
+
 public class Observation {
+
+        private int BirdId;
+        private String Comment;
+        private Calendar Created;
+        private int Id;
+        private double Latitude;
+        private double Longitude;
+        private String Placename;
+        private int Population;
+        private String UserId;
+        private String NameDanish;
+        private String NameEnglish;
+
+        public Observation(){
+
+        }
+
+        public Observation(int BirdId, String Comment, Calendar Created, int Id, double Latitude, double Longitude, String Placename, int Population, String UserId, String NameDanish, String NameEnglish ){
+
+            this.BirdId = BirdId;
+            this.Comment = Comment;
+            this.Created = Created;
+            this.Id = Id;
+            this.Latitude = Latitude;
+            this.Longitude = Longitude;
+            this.Placename = Placename;
+            this.Population = Population;
+            this.UserId = UserId;
+            this.NameDanish = NameDanish;
+            this.NameEnglish = NameEnglish;
+        }
+
+        public int getBirdId() {
+            return BirdId;
+        }
+
+        public void setBirdId(int birdId) {
+            BirdId = birdId;
+        }
+
+        public String getComment() {
+            return Comment;
+        }
+
+        public void setComment(String comment) {
+            Comment = comment;
+        }
+
+        public Calendar getCreated() {
+            return Created;
+        }
+
+        public void setCreated(Calendar created) {
+            Created = created;
+        }
+
+        public int getId() {
+            return Id;
+        }
+
+        public void setId(int id) {
+            Id = id;
+        }
+
+        public double getLatitude() {
+            return Latitude;
+        }
+
+        public void setLatitude(double latitude) {
+            Latitude = latitude;
+        }
+
+        public double getLongitude() {
+            return Longitude;
+        }
+
+        public void setLongitude(double longitude) {
+            Longitude = longitude;
+        }
+
+        public String getPlacename() {
+            return Placename;
+        }
+
+        public void setPlacename(String placename) {
+            Placename = placename;
+        }
+
+        public int getPopulation() {
+            return Population;
+        }
+
+        public void setPopulation(int population) {
+            Population = population;
+        }
+
+        public String getUserId() {
+            return UserId;
+        }
+
+        public void setUserId(String userId) {
+            UserId = userId;
+        }
+
+        public String getNameDanish() {
+            return NameDanish;
+        }
+
+        public void setNameDanish(String nameDanish) {
+            NameDanish = nameDanish;
+        }
+
+        public String getNameEnglish() {
+            return NameEnglish;
+        }
+
+        public void setNameEnglish(String nameEnglish) {
+            NameEnglish = nameEnglish;
+        }
+
+        @Override
+        public String toString(){
+            return NameDanish;
+        }
 }
+
