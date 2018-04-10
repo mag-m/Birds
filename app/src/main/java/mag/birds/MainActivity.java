08bc36c09a0725f5633b67a7e9aa397c10997fe0
Mag <magnusemortensen95@gmail.com>
2018-04-10 18:32:32 +0200

    added appbarbutton + logout function

--- app/src/main/java/mag/birds/MainActivity.java
+++ app/src/main/java/mag/birds/MainActivity.java
@@ -1,7 +1,14 @@
 package mag.birds;
 
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toolbar;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +16,28 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        android.support.v7.widget.Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId()){
+            case R.id.toolbarLogout:
+                FirebaseAuth.getInstance().signOut();
+                finish();
+        }
+
+
+        return super.onOptionsItemSelected(item);
     }
 }
